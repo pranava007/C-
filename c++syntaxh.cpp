a04d4294054bedc1b7e6cfb23627945c8581a04b
pranava muthu <103396008+pranava007@users.noreply.github.com>
2022-12-09 07:35:43 +0530

    c++

---
+++ c++syntaxh.cpp
@@ -0,0 +1,16 @@
+#include<iostream> 
+//iostream one file io=input output stream 
+
+int main()
+ //defult main function c++ program writte all time must importan int main() 
+//int=> return panna use pannanum
+
+{
+
+std::cout<<"hello";
+//<<stream instructor operater
+//oru function la instraction kudutha ; use pannanum program end pannum
+//
+
+    return 0;
+}
