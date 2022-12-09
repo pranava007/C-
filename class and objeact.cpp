f386d3327c0b1a1d3139e5c10bdab51991cbbc7e
pranava muthu <103396008+pranava007@users.noreply.github.com>
2022-12-09 07:36:56 +0530

    c++

---
+++ class and objeact.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+class cool
+{
+      public:
+void sum(int a,int b)
+{
+std::cout<<a+b;
+}
+//2nd function
+void thum(int a,int b)
+{
+std::cout<<a+b;
+}
+
+};
+int main()
+{
+    cool object;
+    cool object1;
+    object.sum(10,2000);
+
+    object.thum(10,1000);
+
+    object.sum(10,2033);
+
+    object.sum(20,20);
+
+    return 0; 
+}
+
+
+
+
+
+/*
+
+grouping working
+ora consapt la more 10 function na  writte panni grouping panni work panna class use akuthu
+public: access specifier out said la eruthu call panna lam
+
+one class more than objeact use it
+one bulprint more than home make
+
+//-------------------------------------------------
+//-------------------------------------------------
+
+syntax
+class name 
+{
+public:
+void name(argument)
+{
+condition
+statament
+}
+
+};
+int main()
+{
+    name objact;
+    objact.name(assin value,);
+    return 0;
+}
+
+*/
