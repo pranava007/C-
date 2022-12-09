cfab90f2f48d76847077ad5453d9958a957eed17
pranava muthu <103396008+pranava007@users.noreply.github.com>
2022-12-09 07:12:49 +0530

    c++

---
+++ Arithimatic oprater.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+int main()
+{
+
+int a,b,c;
+a=5+20;
+b=10;
+c;
+c=a/b;
+std::cout<<c;
+
+    return 0;
+}
+
+// +,-,*,=,/, %=> reminde,() modules operater
+//BODMADS
+//B=> () FIRST PREFERNECE  ,O Oders ,Diviton == divition  left to right, addition == subraction
