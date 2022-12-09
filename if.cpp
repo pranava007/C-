4ce4cf20371ad4b76613c4d11be0fefa33523908
pranava muthu <103396008+pranava007@users.noreply.github.com>
2022-12-09 07:38:59 +0530

    c++

---
+++ if.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+int main()
+{
+    int a,b;
+    a=10;
+    b=20;
+
+if(a<=b)
+{
+std::cout<<"Done";
+}
+    return 0;
+}
+
+/*
+one intraction
+condition
+true run 
+flase not run
+*/
+
+
+//!= not equal 
+
+/*real world exsample =>
+user id and password crt da erutha go to profile
+*/
+
+/*syntax
+if()
+{
+    std::cout<<"";
+}*/
+
