7fa0009b8d0a8cee4a0665ba1d3fff6ad3f96c5a
pranava muthu <103396008+pranava007@users.noreply.github.com>
2022-12-09 07:37:39 +0530

    c++

---
+++ getting input.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+int main()
+{
+    int a,b,c;
+    a+b*c;
+
+    std::cout<< "enter tha a value :" ;
+    std::cin>>a;
+
+     std::cout<< "enter tha b value :" ;
+     std::cin>>b;
+
+
+     std::cout<< "enter tha c value :" ;
+     std::cin>>c;
+
+     std::cout<<"Total" << a+b*c;
+     //>>stream extract oprater 
+
+    return 0;
+}
