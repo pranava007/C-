9bfb9fe64d10e51e29b4a559884c5594ee2d379d
pranava muthu <103396008+pranava007@users.noreply.github.com>
2022-12-09 07:13:12 +0530

    c++

---
+++ basic function.cpp
@@ -0,0 +1,107 @@
+
+#include<iostream>
+int number()
+{
+    int a=10;
+    int b=20000000;
+   
+    a+b;
+    std::cout<<a+b;
+   
+}
+int main()
+{
+    number();
+    return 0;
+}
+
+
+/*
+int main() one  of the function defult function
+
+own function na creat pannalam
+
+int main()=> value va return pannum
+void return pannathu
+
+computer ra oru valaya saya sonna athan function
+C++ 1000 more then function na writte pannalam
+
+return 0; => code run ana aprom value va return pannum
+
+void cout eruckatha print pannum;
+
+
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+
+syntax
+1 TYPE
+#include<iostream>
+void name()
+{
+   
+}
+int main()
+
+{
+     name();
+    return 0;
+}
+2ND TYPE
+
+#include<iostream>
+int number()
+{
+    int a=;
+    int b=;
+   
+    a+b;
+   
+   
+}
+int main()
+{
+    number();
+    return 0;
+}
+*/
+
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+
+
+
+
+//ethu oru type
+
+
+/*#include<iostream>
+void hello()
+{
+    std::cout<<"cool";
+}
+int main()
+
+{
+     hello();
+    return 0;
+}
+*/
+
+// ethu oru type
+/*
+#include<iostream>
+void hello();
+int main()
+{
+    hello();
+    return 0;
+}
+
+void hello()
+{
+    std::cout<<"masss";
+}
+*/
+
