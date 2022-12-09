a09ec2e13b7dce2101a6c2d977a0c0b145f56095
pranava muthu <103396008+pranava007@users.noreply.github.com>
2022-12-09 07:38:21 +0530

    c++

---
+++ if else.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+int main()
+{
+    int a,b;
+    a=10;
+    b=20;
+    if(a==b)
+    {
+     std::cout<<"payment";
+    }
+    else
+    {
+     std::cout<<"login";
+    }
+    return 0;
+}
+
+/*
+
+
+snytax
+if()
+{
+    std::cout<<"";
+}
+else
+{
+    std::cout<<"";
+}
+
+real world exsamble
+cours buy pantraga but login pannala so login page pokummm sobous login alrady pannirutha payment excute akum
+
+*/
