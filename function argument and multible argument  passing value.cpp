95892cfd355cb7cad7e39c04d2965b424cf4162d
pranava muthu <103396008+pranava007@users.noreply.github.com>
2022-12-09 07:37:17 +0530

    c++

---
+++ function argument and multible argument  passing value.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+void name(int a,int b)
+{
+    std::cout<<a+b*b;
+}
+int main()
+{
+
+    name(10,200000);
+    return 0;
+}
+
+
+/*
+one of the function 
+argument function
+passing values 
+
+syntax
+
+#include<iostream>
+void name(argument)
+{
+    
+}
+int main()
+{
+
+    name(passing value);
+    return 0;
+}
+
+*/
