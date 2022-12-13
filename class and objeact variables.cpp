90b44ab1c920aa6af087da2eeddf5c8b732dc1aa
pranava muthu <103396008+pranava007@users.noreply.github.com>
2022-12-13 22:08:55 +0530

    c++

---
+++ class and objeact variables.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+class Shop
+{
+    private:
+    
+        int money;
+        public:
+        void modifemoney()
+        {
+            money = money + 5;
+        } 
+
+        int getmoney()
+        {
+            
+            return money;
+        }       
+    
+
+
+
+
+};
+int main()
+{
+
+Shop op;
+op.getmoney();
+std::cout<<op.getmoney();
+
+
+    return 0;
+}
